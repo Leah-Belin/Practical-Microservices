e1792caadd43e0802b3c70df2426aa0ec8009925
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 14:51:18 -0400

    fixing typos

--- src/message-store/write.js
+++ src/message-store/write.js
@@ -1,42 +1,43 @@
-const writeFuncitonSql = 'SELECT message_store.write_message($1, $2, $3, $4, $5, $6)'
+const writeFunctionSql = 'SELECT message_store.write_message($1, $2, $3, $4, $5, $6)'
 const VersionConflictError = require('./version-conflict-error')
 const versionConflictErrorRegex = /^Wrong.*Stream Version: (\d+)\)/
 
 function createWrite ({ db }) {
-    return  (streamName, message, expectedVersion) => {
-        if (!message.type) {
-            throw new Error('Messages must have a type')
+  return (streamName, message, expectedVersion) => {
+    if (!message.type) {
+      throw new Error('Messages must have a type')
+    }
+
+    const values = [
+      message.id,
+      streamName,
+      message.type,
+      message.data,
+      message.metadata,
+      expectedVersion
+    ]
+
+    return db.query(writeFunctionSql, values)
+      .catch(err => {
+        const errorMatch = err.message.match(versionConflictErrorRegex)
+        const notVersionConflict = errorMatch === null
+
+        if (notVersionConflict) {
+          throw err
         }
 
-        const values = [
-            message.id,
-            streamName,
-            message.type,
-            message.data,
-            message.metadata,
-            expectedVersion
-        ]
+        const actualVersion = parseInt(errorMatch[1], 10)
 
-        return db.query(writeFunctionSql, values)
-        .catch(err => {
-            const errorMatch  = err.message.match(versionConflictErrorRegex)
-            const notVersionConflict = errorMatch === null
+        const versionConflictError = new VersionConflictError(
+          streamName,
+          actualVersion,
+          expectedVersion
+        )
+        versionConflictError.stack = err.stack
 
-            if (notVersionConflict) {
-                throw err
-            }
-
-            const actualVersion = parseInt(errorMatch[1], 10)
-            const VersionConflictError = new VersionConflictError(
-                streamName,
-                actualVersion,
-                expectedVersion
-            )
-            VersionConflictError.stack = err.stack
-
-            throw VersionConflictError
-        })
-    }
+        throw versionConflictError
+      })
+  }
 }
 
-module.exports = createWrite 
+module.exports = createWrite
