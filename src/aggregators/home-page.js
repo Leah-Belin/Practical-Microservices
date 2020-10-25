8feec029a8784c492e4d3654dcc77f13270da5ae
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 15:18:36 -0400

    fixing typos in queries

--- src/aggregators/home-page.js
+++ src/aggregators/home-page.js
@@ -1,73 +1,72 @@
 function createHandlers ({ queries }) {
     return {
-        videoViewed: event => queries.incrementVideosWatched(event.globalPosition)
+      VideoViewed: event => queries.incrementVideosWatched(event.globalPosition)
     }
-}
-
-function createQueries ({ db}) {
-    function ensureHomePage() {
-        const intialData = {
-            pageData: { latViewProcessed: 0, videosWatched: 0}
-        }
-
-        const queryString = `
+  }
+    
+  function createQueries ({ db }) {
+    function ensureHomePage () {
+      const initialData = {
+        pageData: { lastViewProcessed: 0, videosWatched: 0 }
+      }
+  
+      const queryString = `
         INSERT INTO
           pages(page_name, page_data)
         VALUES
           ('home', :pageData)
         ON CONFLICT DO NOTHING
       `
-
-      return db.then(client => client.raw(queryString, intialData))
+  
+      return db.then(client => client.raw(queryString, initialData))
     }
-
+  
     function incrementVideosWatched (globalPosition) {
-        const queryString = `
+      const queryString = `
         UPDATE
-        pages
-      SET
-        page_data = jsonb_set(
-          jsonb_set(
-            page_data,
-            '{videosWatched}',
-            ((page_data ->> 'videosWatched')::int + 1)::text::jsonb
-          ),
-          '{lastViewProcessed}',
-          :globalPosition::text::jsonb
-        )
-      WHERE
-        page_name = 'home' AND
-        (page_data->>'lastViewProcessed')::int < :globalPosition
-    `
-    
-        return db.then(client => client.raw(queryString, {globalPosition}))
+          pages
+        SET
+          page_data = jsonb_set(
+            jsonb_set(
+              page_data,
+              '{videosWatched}',
+              ((page_data ->> 'videosWatched')::int + 1)::text::jsonb
+            ),
+            '{lastViewProcessed}',
+            :globalPosition::text::jsonb
+          )
+        WHERE
+          page_name = 'home' AND
+          (page_data->>'lastViewProcessed')::int < :globalPosition
+      `
+  
+      return db.then(client => client.raw(queryString, { globalPosition }))
     }
-
+  
     return {
-        ensureHomePage,
-        incrementVideosWatched
+      ensureHomePage,
+      incrementVideosWatched
     }
-}
-
-function build ({ db, messageStore }) {
+  }
+  
+  function build ({ db, messageStore }) {
     const queries = createQueries({ db })
     const handlers = createHandlers({ queries })
-
+  
     function init () {
-
-        return queries.ensureHomePage()
+      return queries.ensureHomePage()
     }
-
-    function start () {
-        init().then(subscription.start)
+  
+    function start () { 
+      init().then(subscription.start)
     }
-
+  
     return {
-        queries, 
-        handlers,
-        init,
-        start
+      queries,
+      handlers,
+      init,
+      start
     }
-}
-
-module.exports = build
+  }
+  
+  module.exports = build
