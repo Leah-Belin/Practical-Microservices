8ed43e8c34924bb41af7fcc19e545756cbf2c4f8
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-24 20:22:39 -0400

    adding knex migration

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -16,7 +16,9 @@ function createHandlers ({ queries }){
 
 function createQueries ({ db }){
     function loadHomePage () {
-        return db.then(client => client('videos').sum('view_count as videosWatched').then(rows => rows[0]))
+        return db.then(client => client('videos')
+        .sum('view_count as videosWatched')
+        .then(rows => rows[0]))
     }
 
     return {
