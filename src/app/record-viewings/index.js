569fb9cd056bfab626d55100ddd3fd5d5ba850bc
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-24 21:58:53 -0400

    returning from recordViewing or endpoint crashes

--- src/app/record-viewings/index.js
+++ src/app/record-viewings/index.js
@@ -1,33 +1,41 @@
 const express = require('express')
 
-function createActions ({
+function createActions ({ db }) {
+  function recordViewing (traceId, videoId) {
+    // Return something Promise-based so that the endpoint doesn't crash
+    return Promise.resolve(true)
+  }
 
-}){
-    return{
-
-    }
+  return {
+    recordViewing
+  }
 }
 
-function createHandlers ({ actions })
-{
-    function handleRecordViewing (req, res){
-        return actions.recordViewing(req.context.traceId, req.params.videoId).then(() => res.redirect('/'))
-    }
+function createHandlers ({ actions }) {
+  function handleRecordViewing (req, res) {
+    return actions
+      .recordViewing(req.context.traceId, req.params.videoId)
+      .then(() => res.redirect('/'))
+  }
 
-    return {
-        handleRecordViewing
-    }
+  return {
+    handleRecordViewing
+  }
 }
 
-function createRecordViewings ({
+function createRecordViewings ({ 
+  db
+}) {
+  const actions = createActions({
+    db
+  })
+  const handlers = createHandlers({ actions }) 
 
-}){
-    const handlers = createHandlers({ actions })
-    const router = express.Router()
+  const router = express.Router() 
 
-    router.route('/:videoId').post(handlers.handleRecordViewing)
+  router.route('/:videoId').post(handlers.handleRecordViewing) 
 
-    return { actions, handlers, router }
+  return { actions, handlers, router }
 }
 
 module.exports = createRecordViewings
