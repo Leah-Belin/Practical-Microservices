697abc83659f4a0d69af2651467dacb1bfdf10fe
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 14:36:39 -0400

    adding aggregators to config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const createHomeApp = require('./app/home')
 const createRecordViewingsApp = require('./app/record-viewings')
 const createPostgresClient = require('./postgres-client') 
 const createMessageStore = require('./message-store')
+const createHomePageAggregator = require('./aggregators/home-page')
 
 function createConfig ({ env }) {
   const knexClient = createKnexClient({ 
@@ -14,13 +15,26 @@ function createConfig ({ env }) {
   const messageStore = createMessageStore({ db: postgresClient })
   const homeApp = createHomeApp({ db: knexClient }) 
   const recordViewingsApp = createRecordViewingsApp({ messageStore })
+  const homePageAggregator = createHomePageAggregator({
+    dn: knexClient,
+    messageStore
+  })
+  const aggregators = [
+
+  ]
+  const components = [
+    
+  ]
 
   return {
     env,
     db: knexClient,
     homeApp,
     recordViewingsApp,
-    messageStore
+    messageStore,
+    homePageAggregator,
+    aggregators,
+    components
   }
 }
 
