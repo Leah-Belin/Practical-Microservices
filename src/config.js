0cae2009a0cf7bffb1bdf85924bed71c60dbb8b7
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 19:39:32 -0400

    adding homePageAggregator to aggregators

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
-const createKnexClient = require('./knex-client') 
+const createKnexClient = require('./knex-client')
 const createHomeApp = require('./app/home')
 const createRecordViewingsApp = require('./app/record-viewings')
 const createPostgresClient = require('./postgres-client') 
-const createMessageStore = require('./message-store')
+const createMessageStore = require('./message-store') 
 const createHomePageAggregator = require('./aggregators/home-page')
 
 function createConfig ({ env }) {
-  const knexClient = createKnexClient({ 
+  const knexClient = createKnexClient({
     connectionString: env.databaseUrl
   })
-  const postgresClient = createPostgresClient({
+  const postgresClient = createPostgresClient({ 
     connectionString: env.messageStoreConnectionString
   })
-  const messageStore = createMessageStore({ db: postgresClient })
-  const homeApp = createHomeApp({ db: knexClient }) 
-  const recordViewingsApp = createRecordViewingsApp({ messageStore })
+  const messageStore = createMessageStore({ db: postgresClient }) 
+  const homeApp = createHomeApp({ db: knexClient })
+  const recordViewingsApp = createRecordViewingsApp({ messageStore }) 
   const homePageAggregator = createHomePageAggregator({
-    dn: knexClient,
+    db: knexClient,
     messageStore
   })
   const aggregators = [
-
+    homePageAggregator
   ]
   const components = [
     
