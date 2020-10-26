1f6bf3bcd4f2526b600ecb4ae76980d775ec5f06
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 21:14:15 -0400

    connect subscriptions and read functions

--- src/message-store/index.js
+++ src/message-store/index.js
@@ -1,11 +1,22 @@
 const createWrite = require('./write')
+const createRead = require('./read')
+const configureCreateSubscription = require('./subscribe')
 const VersionConflictError = require('./version-conflict-error')
 
 function createMessageStore ({ db }) {
     const write = createWrite({ db })
+    const read = createRead({ db })
+    const createSubscription = configureCreateSubscription({
+        read: read.read,
+        readLastMessage: read.readLastMessage,
+        write: write
+    })
 
     return {
-        write: write
+        write: write,
+        createSubscription, 
+        read: read.read,
+        readLastMesage: read.readLastMessage
     }
 } 
 
