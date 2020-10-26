47081a3cb663beaddf27e1ef77f8c84ca7d01aa7
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 22:19:19 -0400

    reverting back to the provided docker config files

--- script/install-message-store-in-heroku.js
+++ script/install-message-store-in-heroku.js
@@ -1,3 +1,17 @@
+/***
+ * Excerpted from "Practical Microservices",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/egmicro for more book information.
+***/
+/*
+Normally we could just use the install.sh script in the Eventide
+Message Store repo.  However, those scripts need to create a database and a
+role.  Heroku doesn't allow us to do those functions, so we have this script.
+It just executes the scripts that run after database and role creation.
+*/
 const childProcess = require('child_process')
 const fs = require('fs')
 const path = require('path')
