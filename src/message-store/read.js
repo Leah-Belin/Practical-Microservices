0a247517dad50f6e5cfe7d102bdfe5cc94ecd1dc
lbelin@homesnap.com <lbelin@homesnap.com>
2020-10-25 21:10:45 -0400

    function to get messages from a stream

--- src/message-store/read.js
+++ src/message-store/read.js
@@ -1,13 +1,35 @@
 const deserializeMessage = require('./deserialize-message')
 const getLastMessageSql = 'SELECT * FROM get_last_stream_message($1)'
+const getCategoryMessagesSql = 'SELECT * FROM get_category_messages($1, $2, $3,)'
+const getStreamMessagesSql = 'SELECt * FROM get_stream_messages($1, $2, $3)'
+
 
 function createRead ({ db }){
+    function read (streamName, fromPosition = 0, maxMessages = 1000){
+        let query = null //<callout id="comessageStore.read.bookkeeping />
+        let values = []
+
+        if (streamName.includes('-')) {
+            //Entity streams have a dash
+            query = getStreamMessagesSql;
+            values = [streamName, fromPosition, maxMessages]
+        } else {
+            //Category streams do not have a dash
+            query = getCategoryMessagesSql
+            values = [streamName, fromPosition, maxMessages]
+        }
+
+        return db.query(query, values)
+            .then(res => res.rows.map(deserializeMessage))
+    }
     function readLastMessage (streamName) {
         return db.query(getLastMessageSql, [ streamName ])
             .then(res => deserializeMessage(res.rows[0]))
     }
-
-    return readLastMessage
+    return {
+        read, 
+        readLastMessage
+    }
 }
 
 function createRead({ db }) {
